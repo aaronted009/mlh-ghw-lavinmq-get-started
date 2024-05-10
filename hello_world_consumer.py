d769cc244a533a7c2b0c60f21d3ab11669467b76
Aaron <aaronpromail@gmail.com>
2024-05-11 00:58:27 +0100

    creating the consumer, its connection with the server and a channel

---
+++ hello_world_consumer.py
@@ -0,0 +1,15 @@
+import pika, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Access the CLOUDAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get("CLOUDAMQP_URL", "amqp://guest:guest@localhost:5672/%2f")
+
+# Create a connection
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+
+# Create a channel
+channel = connection.channel()
+print("[✅] Channel over a connection created")
