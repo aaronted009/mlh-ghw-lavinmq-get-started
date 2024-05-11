bdbfbcf92034eb0948d71001009a2c6599ca1039
Aaron <aaronpromail@gmail.com>
2024-05-11 01:32:32 +0100

    declaring queue

--- hello_world_consumer.py
+++ hello_world_consumer.py
@@ -13,3 +13,8 @@ connection = pika.BlockingConnection(params)
 # Create a channel
 channel = connection.channel()
 print("[✅] Channel over a connection created")
+
+# Declare a queue
+channel.queue_declare(
+    queue="hello_world"
+)  # This is idempotent : meaning we could be not declaring this, as a queue for the producer has already been created and queue can only be declared once (regardless of how many times we run the command, only one queue will be created). This is still declared as the consumer process can be started first.
