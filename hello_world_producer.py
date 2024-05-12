212ff4f68124e59d49175e39d148f3660f72523b
Aaron <aaronpromail@gmail.com>
2024-05-12 19:39:35 +0100

    refactoring producer

--- hello_world_producer.py
+++ hello_world_producer.py
@@ -15,22 +15,27 @@ channel = connection.channel()
 print("[✅] Channel over a connection created")
 
 # Create a direct exchange called "slack_notifications"
-channel.exchange_declare("slack_notifications", "direct")
+exchange_name = "slack_notifications"
+channel.exchange_declare(exchange_name, "direct")
 
 # Declaring queues
-channel.queue_declare(queue="hr_queue")
-channel.queue_declare(queue="marketing_queue")
-channel.queue_declare(queue="support_queue")
+queues = {"hr": "hr_queue", "support": "support_queue", "marketing": "marketing_queue"}
+for _, queue_name in queues.items():
+    channel.queue_declare(queue=queue_name, durable=True)
+
 
 # Bind the queues to the exchange
-channel.queue_bind("hr_queue", "slack_notifications", "hr")
-channel.queue_bind("marketing_queue", "slack_notifications", "marketing")
-channel.queue_bind("support_queue", "slack_notifications", "support")
+for binding_key, queue_name in queues.items():
+    channel.queue_bind(
+        exchange=exchange_name, queue=queue_name, routing_key=binding_key
+    )
 
 
 def send_to_queue(channel, routing_key, body):
     """Send message to queue."""
-    channel.basic_publish(exchange="slack_notifications", routing_key=routing_key, body=body)
+    channel.basic_publish(
+        exchange=exchange_name, routing_key=routing_key, body=body
+    )
     print(f"[📥] Message sent to queue - msg:  #{body}")
 
 
